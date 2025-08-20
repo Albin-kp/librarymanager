847ba2771ba318ae370d0716c697f6e0c4fa2c71
Albin-kp <albinkp90@gmail.com>
2025-08-20 21:29:53 +0530

    updated title

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3258935154"
+    serviceWorkerVersion: "634370425"
   }
 });
